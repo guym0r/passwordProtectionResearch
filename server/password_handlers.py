2d24a05351d60bca88d6331136c8eb68bf32816e
Guy Moria <guym@Guys-MacBook-Pro.local>
2026-01-03 13:23:53 +0200

    add suppoer for pepper

--- server/password_handlers.py
+++ server/password_handlers.py
@@ -2,7 +2,7 @@
 import hashlib
 import secrets
 
-def prepare_plaintext_handler(is_new_user, stored_password=None):
+def prepare_plaintext_handler(is_new_user, stored_password=None, config=None):
     """Prepare function for plaintext handler - returns empty dict"""
     return {}
 
@@ -10,25 +10,36 @@ def plaintext_handler(password, handler_info):
     """Plaintext handler - returns password as-is"""
     return password
 
-def prepare_sha256_salt_handler(is_new_user, stored_password=None):
+def prepare_sha256_salt_handler(is_new_user, stored_password=None, config=None):
     """Prepare function for sha256-salt handler"""
+    handler_info = {}
+    
     if is_new_user:
         # Generate random salt for new user
         salt = secrets.token_hex(16)  # 32 hex characters (16 bytes)
-        return {'salt': salt}
+        handler_info['salt'] = salt
     else:
         # Extract salt from stored password (format: salt:hash)
         if stored_password and ':' in stored_password:
             salt, _ = stored_password.split(':', 1)
-            return {'salt': salt}
+            handler_info['salt'] = salt
         else:
             raise ValueError('Invalid stored password format for sha256-salt')
+    
+    # Add pepper if USE_PEPPER is enabled
+    if config and config.get('USE_PEPPER', False):
+        handler_info['pepper'] = config.get('PEPPER', '')
+    
+    return handler_info
 
 def sha256_salt_handler(password, handler_info):
     """SHA256 handler with salt - concatenates salt to password and hashes"""
     salt = handler_info['salt']
-    # Concatenate salt to password
-    salted_password = password + salt
+    pepper = handler_info.get('pepper', '')
+    
+    # Concatenate password + pepper + salt
+    salted_password = password + pepper + salt
+    
     # Hash with SHA256
     hash_obj = hashlib.sha256(salted_password.encode('utf-8'))
     hash_hex = hash_obj.hexdigest()
